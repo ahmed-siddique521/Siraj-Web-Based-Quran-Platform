1c4765f8beb10204270b6e05d78426558808e89f
Ahmed Siddique <ahmedsiddique521@gmail.com>
2026-09-01 19:43:00 +0500

    Updated for deployment on vercel

--- server/server.js
+++ server/server.js
@@ -20,5 +20,7 @@ app.use("/api/quran", quranRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT} `);
 });
+
+module.exports = app;
